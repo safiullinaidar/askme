2ec85a8e319113422448c034894bd6d8cc30fc29
Aidar Safiullin <aydarin.tatarin@gmail.com>
2022-05-03 23:28:14 +0300

    Validations & callbacks added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,10 +5,47 @@ class UsersController < ApplicationController
   end
 
   def create
-    user_params = params.require(:user).permit(:name, :nickname, :email, :password)
+    @user = User.new(user_params)
 
-    User.create(user_params)
+    if @user.save
+      redirect_to root_path, notice: 'Вы успешно зарегистрировались!'
+    else
+      flash.now[:alert] = 'Вы неправильно заполнили поля формы регистрации!'
 
-    redirect_to root_path, notice: 'Вы успешно зарегистрировались!'
+      render :new
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to root_path, notice: 'Данные пользователя обновлены'
+    else
+      flash.now[:alert] = 'При попытке сохранить пользователя возникли ошибки'
+
+      render :edit
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+
+    session.delete(:user_id)
+
+    redirect_to root_path, notice: 'Пользователь удалён'
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(
+      :name, :nickname, :email, :password, :password_confirmation
+    )
   end
 end
