aaab87d1a821308d6fc0f4b2a645604e9157a553
Aidar Safiullin <aydarin.tatarin@gmail.com>
2022-05-04 22:01:43 +0300

    Redirection after registration fixed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,8 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
+      session[:user_id] = @user.id
+
       redirect_to root_path, notice: 'Вы успешно зарегистрировались!'
     else
       flash.now[:alert] = 'Вы неправильно заполнили поля формы регистрации!'
