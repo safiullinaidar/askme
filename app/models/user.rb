aebd6e603f70f0a854f48e918622d35525807ffc
Aidar Safiullin <aydarin.tatarin@gmail.com>
2022-05-07 19:01:39 +0300

    Fixing small bugs in validation helpers

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,15 @@
 class User < ApplicationRecord
   has_secure_password
 
-  before_save :downcase_nickname
-
   validates :email, 
     presence: true, 
-    uniqueness: true,
-    email: {mode: :strict, require_fqdn: true}
+    uniqueness: { case_sensitive: false },
+    email: { mode: :strict, require_fqdn: true }
   
   validates :nickname, 
     uniqueness: true, 
     length: { maximum: 40 }, 
-    format: { with: /\A[a-z0-9_]+\z/ }
+    format: { with: /\w+\z/ }
 
   def downcase_nickname
     nickname.downcase!
